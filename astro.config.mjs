5ab9a5b19483a5adc0296b85a625b090d14d5825
gviaud <gaetan.viaud@lycee-elie-vinet.org>
2025-03-12 09:49:41 +0100

    Configure Astro pour GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Galeduc.github.io',
+  base: '/Portfolio_Epreuve',
   devToolbar: {
     enabled: false
   },
-  integrations: [react(), tailwind(), sitemap()]
+  integrations: [react(), tailwind(), sitemap()],
 });

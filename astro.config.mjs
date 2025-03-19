f7b91139dee1938c716950d7753b59a301724d9e
gviaud <gaetan.viaud@lycee-elie-vinet.org>
2025-03-19 08:38:18 +0100

    Update Astro config for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
 
 export default defineConfig({
-  site: 'http://galeduc.works', // Assurez-vous d'utiliser HTTPS
-  base: '/', // Ajoutez cette ligne si vous utilisez un domaine personnalisé
-  integrations: [react(), tailwind(), sitemap()],
+    integrations: [react(), tailwind(), sitemap()],
+    output: 'server',
+    adapter: node({
+        mode: 'standalone'
+    }),
 });

a6532e3d2176f002dcfd8dd18b6ef945a68d9d3b
gviaud <gaetan.viaud@lycee-elie-vinet.org>
2025-03-24 13:24:06 +0100

    Update 1

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,8 +1,9 @@
 export const mainMenu = [
      {link:"#", content:"Accueil", icon:"lucide:home"},
+     {link:"#about", content:"A propos de moi", icon:"carbon:portfolio"},
+     {link:"#skills", content:"Mes Competence", icon:"octicon:person-24"},
      {link:"#portafolio", content:"Mes Projets", icon:"octicon:person-24"},
-     {link:"#portafolio", content:"A propos de moi", icon:"carbon:portfolio"},
-     {link:"#blog", content:"Contact", icon:"mdi:blog"}
+     {link:"#contact", content:"Contact", icon:"mdi:blog"}
 ]
 export const menuBlog = [
      {link:"#", content:"Accueil", icon:"lucide:home"},
